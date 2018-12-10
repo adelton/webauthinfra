36f52f44f57ffa3143eeaa1a27e86b5e7286b786
Jan Pazdziora <jpazdziora@redhat.com>
2018-12-10 12:17:15 +0100

    Fixing typo.

--- src/test-saml.py
+++ src/test-saml.py
@@ -1,4 +1,4 @@
-#!/usr/bin.python3
+#!/usr/bin/python3
 
 from sys import argv
 from xvfbwrapper import Xvfb
--- tests/test.sh
+++ tests/test.sh
@@ -10,12 +10,12 @@ docker cp src/www-proxy-gssapi.conf $WWW:/data/www.conf
 docker exec -ti $WWW systemctl restart httpd
 
 # docker cp src/test-kerberos.py $CLIENT:/usr/local/bin/
-docker exec $CLIENT runuser -u developer -- python3 /usr/local/bin/test-kerberos.py $( cat ipa-data/admin-password )
+docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-kerberos.py $( cat ipa-data/admin-password )
 
 docker cp src/www-proxy-saml.conf $WWW:/data/www.conf
 docker exec -ti $WWW systemctl restart httpd
 
 # docker cp src/test-saml.py $CLIENT:/usr/local/bin/
-docker exec $CLIENT runuser -u developer -- python3 /usr/local/bin/test-saml.py $( cat ipa-data/admin-password )
+docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-saml.py $( cat ipa-data/admin-password )
 
 echo OK $0.
