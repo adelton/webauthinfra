30e61be9a6aa7f71ea3594aebf0a79acb9eb4694
Jan Pazdziora <jpazdziora@redhat.com>
2020-11-19 19:29:26 +0100

    Enable testing with GitHub Actions.

--- tests/test.sh
+++ tests/test.sh
@@ -12,7 +12,7 @@ if test -n "$WWWAPP" ; then
 else
 	docker cp src/www-proxy-gssapi.conf $WWW:/data/www.conf
 fi
-docker exec -ti $WWW systemctl restart httpd
+docker exec $WWW systemctl restart httpd
 
 # docker cp src/test-kerberos.py $CLIENT:/usr/local/bin/
 docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-kerberos.py $( cat ipa-data/admin-password )
@@ -22,7 +22,7 @@ if test -n "$WWWAPP" ; then
 else
 	docker cp src/www-proxy-saml.conf $WWW:/data/www.conf
 fi
-docker exec -ti $WWW systemctl restart httpd
+docker exec $WWW systemctl restart httpd
 
 # docker cp src/test-saml.py $CLIENT:/usr/local/bin/
 docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-saml.py $( cat ipa-data/admin-password )
@@ -32,7 +32,7 @@ if test -n "$WWWAPP" ; then
 else
 	docker cp src/www-proxy-openidc.conf $WWW:/data/www.conf
 fi
-docker exec -ti $WWW systemctl restart httpd
+docker exec $WWW systemctl restart httpd
 
 # docker cp src/test-openidc.py $CLIENT:/usr/local/bin/
 docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-openidc.py $( cat ipa-data/admin-password )
