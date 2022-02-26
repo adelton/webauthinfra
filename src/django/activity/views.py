b2ecff3d091c517700597b0a921b869206d5bc07
Jan Pazdziora <jpazdziora@redhat.com>
2022-02-26 14:37:37 +0100

    Update to Django 4.0.

--- src/django/activity/views.py
+++ src/django/activity/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
-from django.utils.http import is_safe_url
+from django.utils.http import url_has_allowed_host_and_scheme
 
 def index(request):
     activity_list = User.objects.order_by('-last_login')[:20]
@@ -27,7 +27,7 @@ def login(request, template_name='activity/login.html', redirect_field_name=REDI
     if hasattr(request, 'user') and request.user.is_authenticated:
         redirect_to = request.POST.get(redirect_field_name,
             request.GET.get(redirect_field_name, ''))
-        if not is_safe_url(url=redirect_to, allowed_hosts=request.get_host()):
+        if not url_has_allowed_host_and_scheme(url=redirect_to, allowed_hosts=request.get_host()):
             redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
         return HttpResponseRedirect(redirect_to)
     return LoginView.as_view(template_name = template_name, redirect_field_name = redirect_field_name)(request)
