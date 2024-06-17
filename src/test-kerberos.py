72933a935ee455073122a9c3023042bfa95e8197
Jan Pazdziora <jpazdziora@redhat.com>
2024-06-17 09:20:59 +0200

    New Selenium compatibility fixes.

--- src/test-kerberos.py
+++ src/test-kerberos.py
@@ -5,61 +5,63 @@ from xvfbwrapper import Xvfb
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.firefox.service import Service
 import re
 
 print("Admin password " + argv[1])
 
 with Xvfb() as xvfb:
 
-	driver = webdriver.Firefox(service_log_path = "/tmp/geckodriver.log")
+	service = Service(executable_path = "/usr/local/bin/geckodriver", log_path = "/tmp/geckodriver.log")
+	driver = webdriver.Firefox(service = service)
 
 	driver.get("http://localhost")
 	print(driver.title)
 	assert "Kerberos setup" in driver.title
 
-	pre = driver.find_element_by_xpath("//pre")
+	pre = driver.find_element("xpath", "//pre")
 	print(pre.text)
 
-	kinit_form = driver.find_element_by_xpath("//form[input[@type = 'hidden'][@name = 'kinit']]")
-	kinit_form.find_element_by_id("username").send_keys("admin")
-	kinit_form.find_element_by_id("password").send_keys(argv[1])
+	kinit_form = driver.find_element("xpath", "//form[input[@type = 'hidden'][@name = 'kinit']]")
+	kinit_form.find_element("id", "username").send_keys("admin")
+	kinit_form.find_element("id", "password").send_keys(argv[1])
 	print("Submitting kinit")
-	html = driver.find_element_by_xpath("/html")
+	html = driver.find_element("xpath", "/html")
 	kinit_form.submit()
 	WebDriverWait(driver, 15).until(expected_conditions.staleness_of(html))
 
-	pre = driver.find_element_by_xpath("//pre")
+	pre = driver.find_element("xpath", "//pre")
 	print(pre.text)
 	assert "Valid starting" in pre.text
 	assert "krbtgt/EXAMPLE.TEST@EXAMPLE.TEST" in pre.text
 
 	driver.get("https://www.example.test/")
-	title = driver.find_element_by_xpath("//h1/a")
+	title = driver.find_element("xpath", "//h1/a")
 	print(title.text)
 	assert "Not logged in" in title.text
 
-	logon_link = driver.find_element_by_xpath("//a[@href][text() = 'login']")
-	html = driver.find_element_by_xpath("/html")
+	logon_link = driver.find_element("xpath", "//a[@href][text() = 'login']")
+	html = driver.find_element("xpath", "/html")
 	logon_link.click()
 	WebDriverWait(driver, 15).until(expected_conditions.staleness_of(html))
 	print(driver.current_url)
 
-	title = driver.find_element_by_xpath("//h1/a")
+	title = driver.find_element("xpath", "//h1/a")
 	print(title.text)
 	assert "Logged in as admin" in title.text
 
-	groups = driver.find_element_by_xpath("//tr[td/text() = 'Member of groups']/td[2]")
+	groups = driver.find_element("xpath", "//tr[td/text() = 'Member of groups']/td[2]")
 	print(groups.text)
 	assert "ext:admins" in groups.text
 
 	driver.get("http://localhost")
 	assert "Kerberos setup" in driver.title
 	print("Submitting kdestroy")
-	html = driver.find_element_by_xpath("/html")
-	driver.find_element_by_name("kdestroy").submit()
+	html = driver.find_element("xpath", "/html")
+	driver.find_element("name", "kdestroy").submit()
 	WebDriverWait(driver, 15).until(expected_conditions.staleness_of(html))
 
-	pre = driver.find_element_by_xpath("//pre")
+	pre = driver.find_element("xpath", "//pre")
 	print(pre.text)
 	assert re.match("klist: No credentials cache found|klist: Credentials cache keyring 'persistent:456:456' not found", pre.text)
 
--- src/test-openidc.py
+++ src/test-openidc.py
@@ -6,34 +6,36 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.service import Service
 
 print("Admin password " + argv[1])
 
 with Xvfb() as xvfb:
 
-	driver = webdriver.Firefox(service_log_path = "/tmp/geckodriver.log")
+	service = Service(executable_path = "/usr/local/bin/geckodriver", log_path = "/tmp/geckodriver.log")
+	driver = webdriver.Firefox(service = service)
 
 	driver.get("https://www.example.test/")
-	title = driver.find_element_by_xpath("//h1/a")
+	title = driver.find_element("xpath", "//h1/a")
 	print(title.text)
 	assert "Not logged in" in title.text
 
-	logon_link = driver.find_element_by_xpath("//a[@href][text() = 'login']")
+	logon_link = driver.find_element("xpath", "//a[@href][text() = 'login']")
 	current_url = driver.current_url
 	logon_link.click()
 	WebDriverWait(driver, 15).until(expected_conditions.url_changes(current_url))
 	print(driver.current_url)
 
-	logon_form = driver.find_element_by_xpath("//form[input[@name = 'ipsilon_transaction_id']]")
-	logon_form.find_element_by_id("login_name").send_keys("admin")
-	logon_form.find_element_by_id("login_password").send_keys(argv[1])
+	logon_form = driver.find_element("xpath", "//form[input[@name = 'ipsilon_transaction_id']]")
+	logon_form.find_element("id", "login_name").send_keys("admin")
+	logon_form.find_element("id", "login_password").send_keys(argv[1])
 	current_url = driver.current_url
 	logon_form.submit()
 	WebDriverWait(driver, 15).until(expected_conditions.url_changes(current_url))
 	print(driver.current_url)
 
 	try:
-		decided_allow = driver.find_element_by_xpath("//form//button[@name = 'decided_allow']")
+		decided_allow = driver.find_element("xpath", "//form//button[@name = 'decided_allow']")
 		decided_allow.click()
 	except NoSuchElementException:
 		pass
@@ -41,11 +43,11 @@ with Xvfb() as xvfb:
 	WebDriverWait(driver, 15).until(expected_conditions.url_to_be("https://www.example.test/"))
 	print(driver.current_url)
 
-	title = driver.find_element_by_xpath("//h1/a")
+	title = driver.find_element("xpath", "//h1/a")
 	print(title.text)
 	assert "Logged in as admin" in title.text
 
-	groups = driver.find_element_by_xpath("//tr[td/text() = 'Member of groups']/td[2]")
+	groups = driver.find_element("xpath", "//tr[td/text() = 'Member of groups']/td[2]")
 	print(groups.text)
 	assert "ext:admins" in groups.text
 
