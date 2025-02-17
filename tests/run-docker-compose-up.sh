61ceb2704e928d51e01b56815a2b324098c4e992
Jan Pazdziora <jan.pazdziora@code.adelton.com>
2025-02-17 15:18:54 +0100

    Use docker-compose for scripting around the containers as well, no need to mix with the standalone docker.

--- tests/run-docker-compose-up.sh
+++ tests/run-docker-compose-up.sh
@@ -6,30 +6,27 @@ DOCKER_COMPOSE=${DOCKER_COMPOSE:-docker-compose}
 $DOCKER_COMPOSE up -d
 $DOCKER_COMPOSE logs -f &
 
-while ! docker ps | grep webauthinfra.www ; do
+WWWX=$( $DOCKER_COMPOSE config --services | grep www )
+while ! $DOCKER_COMPOSE ps $WWWX ; do
 	sleep 5
 done
-docker ps | grep webauthinfra.www | awk '{ print $NF }' | while read i ; do
-	while true ; do
-		sleep 10
-		echo -n "test | Waiting for $i to initialize ... " >&2
-		if docker exec $i systemctl is-active ipsilon-client-configure.service | tee /dev/stderr | grep -q '^active' ; then
-			break
-		fi
-	done
+while true ; do
+	sleep 10
+	echo -n "test | Waiting for $WWWX to initialize ... " >&2
+	if $DOCKER_COMPOSE exec -T $WWWX $i systemctl is-active ipsilon-client-configure.service | tee /dev/stderr | grep -q '^active' ; then
+		break
+	fi
 done
 
-while ! docker ps | grep webauthinfra.client ; do
+while ! $DOCKER_COMPOSE ps client ; do
 	sleep 5
 done
-docker ps | grep webauthinfra.client | awk '{ print $NF }' | while read i ; do
-	while true ; do
-		sleep 10
-		echo -n "test | Waiting for $i to initialize ... " >&2
-		if docker exec $i systemctl is-active setup-authorized-keys.service | tee /dev/stderr | grep -q '^active' ; then
-			break
-		fi
-	done
+while true ; do
+	sleep 10
+	echo -n "test | Waiting for client to initialize ... " >&2
+	if $DOCKER_COMPOSE exec -T client $i systemctl is-active setup-authorized-keys.service | tee /dev/stderr | grep -q '^active' ; then
+		break
+	fi
 done
 
 echo OK $0.
