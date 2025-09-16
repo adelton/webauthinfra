59f40c4f253fa1feebd5e2130a6fbd5ac0649035
Jan Pazdziora <jan.pazdziora@code.adelton.com>
2025-09-16 10:09:15 +0200

    podman-compose does not have the cp subcommand.

--- tests/test.sh
+++ tests/test.sh
@@ -9,27 +9,27 @@ WWWAPP=$( $DOCKER_COMPOSE config --services | grep -Fx wwwapp )
 
 set -e
 if test -n "$WWWAPP" ; then
-	$DOCKER_COMPOSE cp src/www-mod_wsgi-gssapi.conf wwwapp:/data/www.conf
+	cat src/www-mod_wsgi-gssapi.conf | $DOCKER_COMPOSE exec -T wwwapp cp /dev/stdin /data/www.conf
 else
-	$DOCKER_COMPOSE cp src/www-proxy-gssapi.conf www:/data/www.conf
+	cat src/www-proxy-gssapi.conf | $DOCKER_COMPOSE exec -T www cp /dev/stdin /data/www.conf
 fi
 $DOCKER_COMPOSE exec -T $WWWX systemctl restart httpd
 
 $DOCKER_COMPOSE exec -T client runuser -u developer -- /usr/local/bin/test-kerberos.py $( cat ipa-data/admin-password )
 
 if test -n "$WWWAPP" ; then
-	$DOCKER_COMPOSE cp src/www-mod_wsgi-saml.conf wwwapp:/data/www.conf
+	cat src/www-mod_wsgi-saml.conf | $DOCKER_COMPOSE exec -T wwwapp cp /dev/stdin /data/www.conf
 else
-	$DOCKER_COMPOSE cp src/www-proxy-saml.conf www:/data/www.conf
+	cat src/www-proxy-saml.conf | $DOCKER_COMPOSE exec -T www cp /dev/stdin /data/www.conf
 fi
 $DOCKER_COMPOSE exec -T $WWWX systemctl restart httpd
 
 $DOCKER_COMPOSE exec -T client runuser -u developer -- /usr/local/bin/test-saml.py $( cat ipa-data/admin-password )
 
 if test -n "$WWWAPP" ; then
-	$DOCKER_COMPOSE cp src/www-mod_wsgi-openidc.conf wwwapp:/data/www.conf
+	cat src/www-mod_wsgi-openidc.conf | $DOCKER_COMPOSE exec -T wwwapp cp /dev/stdin /data/www.conf
 else
-	$DOCKER_COMPOSE cp src/www-proxy-openidc.conf www:/data/www.conf
+	cat src/www-proxy-openidc.conf | $DOCKER_COMPOSE exec -T www cp /dev/stdin /data/www.conf
 fi
 $DOCKER_COMPOSE exec -T $WWWX systemctl restart httpd
 
