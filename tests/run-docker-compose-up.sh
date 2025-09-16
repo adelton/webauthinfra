65879d4453ffe88a06474304a2a42f5720b204b7
Jan Pazdziora <jan.pazdziora@code.adelton.com>
2025-09-16 19:25:36 +0200

    Check the systemctl status of all systemd-based containers.

--- tests/run-docker-compose-up.sh
+++ tests/run-docker-compose-up.sh
@@ -5,28 +5,27 @@ DOCKER_COMPOSE=${DOCKER_COMPOSE:-docker-compose}
 
 $DOCKER_COMPOSE up -d
 $DOCKER_COMPOSE logs -f &
+set +e
 
-WWWX=$( $DOCKER_COMPOSE config --services | grep www )
-while ! $DOCKER_COMPOSE ps $WWWX ; do
-	sleep 5
-done
-while true ; do
-	sleep 10
-	echo -n "test | Waiting for $WWWX to initialize ... " >&2
-	if $DOCKER_COMPOSE exec -T $WWWX $i systemctl is-active ipsilon-client-configure.service | tee /dev/stderr | grep -q '^active' ; then
-		break
+RESULT=true
+$DOCKER_COMPOSE config --services | grep -vF app | while read s ; do
+	while ! $DOCKER_COMPOSE ps $s > /dev/null ; do
+		sleep 5
+	done
+	STATUS=offline
+	while true ; do
+		STATUS=$( $DOCKER_COMPOSE exec -T $s systemctl is-system-running --wait < /dev/null )
+		IRESULT=$?
+		if [ "$STATUS" != offline ] ; then break ; fi
+		sleep 5
+	done
+	echo "$s: $STATUS"
+	if [ $IRESULT -ne 0 ] ; then
+		RESULT=false
+		$DOCKER_COMPOSE exec -T $s systemctl status --failed < /dev/null
 	fi
+	$RESULT
 done
-
-while ! $DOCKER_COMPOSE ps client ; do
-	sleep 5
-done
-while true ; do
-	sleep 10
-	echo -n "test | Waiting for client to initialize ... " >&2
-	if $DOCKER_COMPOSE exec -T client $i systemctl is-active setup-authorized-keys.service | tee /dev/stderr | grep -q '^active' ; then
-		break
-	fi
-done
+[ $? -ne 0 ] && exit 1
 
 echo OK $0.
