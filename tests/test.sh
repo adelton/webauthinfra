c7d170b9ab47ca80b70bdfc562bf633c0a15c9f2
Jan Pazdziora <jan.pazdziora@code.adelton.com>
2025-09-17 10:15:15 +0200

    The test-*.py scripts are copied to the client image in src/Dockerfile.client.

--- tests/test.sh
+++ tests/test.sh
@@ -14,7 +14,6 @@ else
 fi
 docker exec $WWW systemctl restart httpd
 
-# docker cp src/test-kerberos.py $CLIENT:/usr/local/bin/
 docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-kerberos.py $( cat ipa-data/admin-password )
 
 if test -n "$WWWAPP" ; then
@@ -24,7 +23,6 @@ else
 fi
 docker exec $WWW systemctl restart httpd
 
-# docker cp src/test-saml.py $CLIENT:/usr/local/bin/
 docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-saml.py $( cat ipa-data/admin-password )
 
 if test -n "$WWWAPP" ; then
@@ -34,7 +32,6 @@ else
 fi
 docker exec $WWW systemctl restart httpd
 
-# docker cp src/test-openidc.py $CLIENT:/usr/local/bin/
 docker exec $CLIENT runuser -u developer -- /usr/local/bin/test-openidc.py $( cat ipa-data/admin-password )
 
 echo OK $0.
